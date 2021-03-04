7c528f0f79f57e76126028cb3ecc93820ca7d1e6
Alonso Uribe <a.uribe_@hotmail.com>
2021-03-03 22:55:17 -0300

    Region data scrapped.
    https://es.wikipedia.org/wiki/Anexo:Comunas_de_Chile page scrapped to retrieve an organized and rich dataset about each commune of Chile.

---
+++ chile_regions.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import numpy as np
+
+rows = []
+with webdriver.Chrome("C:/Users/Alonso Uribe/AppData/Local/Chromium/User Data/chromedriver.exe") as driver:
+        driver.get("https://es.wikipedia.org/wiki/Anexo:Comunas_de_Chile")
+        content = driver.page_source
+        soup = BeautifulSoup(content, 'html.parser')
+table_content = soup.find('table', class_="wikitable sortable jquery-tablesorter")
+table_labels = table_content.find('thead')
+table_labels = [th.text for th in table_labels.find_all('th')]
+table_body = table_content.find('tbody')
+rows = []
+for row in table_body.find_all('tr'):
+    values = [value.text for value in row.find_all('td')]
+    rows.append(values)
+
+#############
+# Pre-process
+#############
+
+df = pd.DataFrame(rows)
+df = df.drop(columns=9)
+table_labels[0] = "CUT"
+table_labels[1] = "Comuna"
+table_labels = [label.strip("\n").lower() for label in table_labels]
+df.columns = table_labels
+df = df.applymap(lambda x: x.strip("\n").strip().lower())
+df[table_labels[5]] = df[table_labels[5]].apply(lambda x: x.replace(".", "").replace(",", "."))
+df[table_labels[6]] = df[table_labels[6]].apply(lambda x: x.replace(".", ""))
+df[table_labels[7]] = df[table_labels[7]].apply(lambda x: x.replace(",", "."))
+df = df.applymap(lambda x: (float(x)) if x.isnumeric() else x)
+df = df.convert_dtypes()
+print(df.head(10))
+for column in df:
+    print(df[column].dtype)
+
+df.to_csv('Data/communes_of_chile.csv')
--- inmobiliary_stadistics.py
+++ inmobiliary_stadistics.py
@@ -11,9 +11,15 @@ import numpy as np
 rows = []
 with webdriver.Chrome("C:/Users/Alonso Uribe/AppData/Local/Chromium/User Data/chromedriver.exe") as driver:
         driver.get("https://www.portalinmobiliario.com/")
+        wait = WebDriverWait(driver, 10)
         cookies = driver.find_element(By.XPATH, "//button[@id='cookieDisclaimerButton']")
         try:
             webdriver.ActionChains(driver).click(cookies).perform()
         except NoSuchElementException:
             pass
-        driver.implicitly_wait(2)
+        driver.implicitly_wait(1)
+        curr_url = driver.current_url
+        driver.find_element(By.XPATH, "//searchbox-button[@id='search-submit']").click()
+        wait.until(EC.url_changes(curr_url))
+
+
