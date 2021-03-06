46421fd52f92713d4c8c556ba86a734d71a6163f
Alonso Uribe <a.uribe_@hotmail.com>
2021-03-06 10:50:23 -0300

    Version 2

--- inmobiliary_stadistics.py
+++ inmobiliary_stadistics.py
@@ -14,47 +14,100 @@ with webdriver.Chrome("C:/Users/Alonso Uribe/AppData/Local/Chromium/User Data/ch
         wait = WebDriverWait(driver, 10)
         cookies = driver.find_element(By.XPATH, "//button[@id='cookieDisclaimerButton']")
         try:
-            webdriver.ActionChains(driver).click(cookies).perform()
+                webdriver.ActionChains(driver).click(cookies).perform()
         except NoSuchElementException:
-            pass
+                pass
         driver.implicitly_wait(1)
+
         curr_url = driver.current_url
-        driver.find_element(By.XPATH, "//searchbox-button[@id='search-submit']").click()
-        wait.until(EC.url_changes(curr_url))
-        # All this identation code belongs to the scrapping of one configuration of higher
-        # variable through all pages displayed to the corresponing variables.
-        max_iter = 100
-        for sell_type in :
-                for property_type in :
-        for i in range(max_iter):
-                # This is the scrapping of just one page of the higher variables's configuration
-                print(f"Scrapping page {i + 1}")
-                content = driver.page_source
-                soup = BeautifulSoup(content, "html.parser")
-                for house in soup.findAll("li", class_="ui-search-layout__item"):
-                        price_elem = house.find("span")
-                        price = [price_elem.text]
-                        features_elems = house.findAll("li")
-                        features = [elem.text for elem in features_elems]
-                        location_elem = house.findAll("p")
-                        location = [elem.text for elem in location_elem]
-                        all = [item.strip().lower() for sublist in [price, features, location] for item in sublist]
-                        rows.append(all)
-                next_page_locator = By.XPATH, "//a[@title='Siguiente' and @role='button']"
-                try:
-                        next_page = driver.find_element(*next_page_locator)  # Two elements with the same class, Previous and Next
-                        # location = {'x': 684, 'y': 10815}
-                        next_url = next_page.get_attribute("href")
-                except NoSuchElementException:
-                        print("All pages seen")
-                        break
-                wait.until(EC.element_to_be_clickable(next_page_locator))
+        #first_search_button = driver.find_element(By.XPATH, "//searchbox-button[@id='search-submit']")
+        #webdriver.ActionChains(driver).move_to_element(first_search_button).click().perform()
+        try:
+                driver.find_element(
+                        By.XPATH,
+                        "//span[@role='button' and @class='andes-tooltip-button-close']") \
+                        .click()
+        except NoSuchElementException:
+                pass
+
+        menu_class = "search-bar " #"ui-search-faceted-search"
+        menu_pointer = driver.find_element(By.XPATH, f"//div[@class='{menu_class}']")
+        sells_pointer, properties_pointer = menu_pointer.find_elements(
+                By.XPATH,
+                f"//div[@class='andes-dropdown andes-dropdown--standalone  searchbox-dropdown ']") #'{menu_class}--searchbox-dropdown']")
+
+        switch = False
+        for sell_type in sells_pointer.find_elements(By.TAG_NAME, 'li'):
                 webdriver.ActionChains(driver) \
-                        .move_to_element(next_page) \
-                        .click(next_page).perform()
-                wait.until(EC.url_to_be(next_url))  # Waiting until it's at the url clicked
-                wait.until(EC.visibility_of_all_elements_located(
-                        (By.XPATH, "//li[@class='ui-search-layout__item']")))  # until all list elements can be seen
-
-
+                        .move_to_element(sells_pointer).click()\
+                        .perform()
+                driver.implicitly_wait(1)
+                types_list = [sell_type.text]
+                webdriver.ActionChains(driver)\
+                        .move_to_element(sell_type).click() \
+                        .perform()
+                for property_type in properties_pointer.find_elements(By.TAG_NAME, 'li'):
+                        search_button = driver.find_element(By.XPATH, "//searchbox-button[@id='search-submit']")
+                        #search_button = menu_pointer.find_element(
+                        #        By.XPATH,
+                        #        "//button[@class='andes-button andes-button--large andes-button--loud']")
+                        act = webdriver.ActionChains(driver) \
+                                .move_to_element(properties_pointer).click() \
+                                .perform()
+                        driver.implicitly_wait(1)
+                        types_list.append(property_type.text)
+                        webdriver.ActionChains(driver) \
+                                .move_to_element(property_type).click() \
+                                .send_keys(Keys.ESCAPE) \
+                                .move_to_element(search_button).click() \
+                                .perform()
+                        wait.until(EC.url_changes(curr_url))
+                        target_url = driver.current_url
+                        # At first iteration ever, this wait won't trigger
+                        driver.back()
+                        original_window = driver.current_window_handle
+                        driver.execute_script("window.open('');")
+                        driver.switch_to.window(driver.window_handles[1])
+                        driver.get(target_url)
+                        wait.until(EC.number_of_windows_to_be(2))
+                        # All this indentated code below belongs to the scrapping of one configuration of higher
+                        # variable through all pages displayed to the corresponing variables.
+                        max_iter=100
+                        for j in range(max_iter):
+                                # This is the scrapping of just one page of the higher variables's configuration
+                                print(f"Scrapping page {j + 1}")
+                                content = driver.page_source
+                                soup = BeautifulSoup(content, "html.parser")
+                                for house in soup.findAll("li", class_="ui-search-layout__item"):
+                                        price_elem = house.find("span")
+                                        price = [price_elem.text]
+                                        features_elems = house.findAll("li")
+                                        features = [elem.text for elem in features_elems]
+                                        location_elem = house.findAll("p")
+                                        location = [elem.text for elem in location_elem]
+                                        all = [item.strip().lower()
+                                               for sublist in [price, features, location, types_list]
+                                               for item in sublist]
+                                        rows.append(all)
+                                next_page_locator = By.XPATH, "//a[@title='Siguiente' and @role='button']"
+                                try:
+                                        next_page = driver.find_element(*next_page_locator)  # Two elements with the same class, Previous and Next
+                                        # location = {'x': 684, 'y': 10815}
+                                        next_url = next_page.get_attribute("href")
+                                except NoSuchElementException:
+                                        print(f"All pages seen from configuration {types_list[0]}-{types_list[1]}")
+                                        break
+                                wait.until(EC.element_to_be_clickable(next_page_locator))
+                                webdriver.ActionChains(driver) \
+                                        .move_to_element(next_page) \
+                                        .click(next_page).perform()
+                                wait.until(EC.url_to_be(next_url))  # Waiting until it's at the url clicked
+                                wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "li"))) # until all list elements can be seen
+                                break # debugging purposes
+                        driver.execute_script("window.close('');")
+                        driver.switch_to.window(original_window)
+                        print(driver.current_url)
+df = pd.DataFrame(rows)
+print(df.iloc[:10,:len(df.columns)])
+print(df.iloc[:10,len(df.columns):])
 
---
+++ whole_scrapping.py
@@ -0,0 +1,131 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import numpy as np
+
+def webelement_to_xpath(webelement, by):
+    name = webelement.tag_name
+    class_ = webelement.get_attribute(by)
+    xpath = f"//{name}[@{by}='{class_}']"
+    return xpath
+def scrapping_all():
+    rows = []
+    with webdriver.Chrome("C:/Users/Alonso Uribe/AppData/Local/Chromium/User Data/chromedriver.exe") as driver:
+        driver.get("https://www.portalinmobiliario.com/")
+        origin_url = driver.current_url
+        wait = WebDriverWait(driver, 10)
+        cookies = driver.find_element(By.XPATH, "//button[@id='cookieDisclaimerButton']")
+        try:
+            webdriver.ActionChains(driver).click(cookies).perform()
+        except NoSuchElementException:
+            pass
+        driver.implicitly_wait(1)
+        try:
+            driver.find_element(
+                By.XPATH,
+                "//span[@role='button' and @class='andes-tooltip-button-close']") \
+                .click()
+        except NoSuchElementException:
+            pass
+
+        menu_class = "search-bar "
+        menu_xpath = f"//div[@class='{menu_class}']"
+        menu_pointer = driver.find_element(By.XPATH, menu_xpath)
+        pointers_class = "andes-dropdown andes-dropdown--standalone  searchbox-dropdown "
+        pointers_xpath = f"//div[@class='{pointers_class}']"
+        sells_pointer, properties_pointer = menu_pointer.find_elements(
+            By.XPATH,
+            pointers_xpath)
+        # Collecting all the posible variables configuration,
+        # then it's just needed to change the respective url
+        sells = []
+        properties = []
+        webdriver.ActionChains(driver).move_to_element(sells_pointer). \
+            click(). \
+            perform()
+        driver.implicitly_wait(1)
+        for sell_type in sells_pointer.find_elements(By.TAG_NAME, 'li'):
+            sells.append(sell_type.text)
+        webdriver.ActionChains(driver).move_to_element(properties_pointer). \
+            click(). \
+            perform()
+        driver.implicitly_wait(1)
+        for property_type in properties_pointer.find_elements(By.TAG_NAME, 'li'):
+            properties.append(property_type.text.lower())
+        webdriver.ActionChains(driver).move_to_element(properties_pointer).send_keys(Keys.ESCAPE).perform()
+
+        for sell in sells:
+            menu_pointer = driver.find_element(By.XPATH, menu_xpath)
+            search_button = menu_pointer.find_element(
+                By.XPATH, "//searchbox-button[@id='search-submit']"
+            )
+            sells_pointer, properties_pointer = menu_pointer.find_elements(By.XPATH, pointers_xpath)
+            print(sells_pointer)
+            webdriver.ActionChains(driver).move_to_element(sells_pointer).click().perform()
+            for x in sells_pointer.find_elements(By.TAG_NAME, "li"):
+                if x.text == sell:
+                    sell_type = x
+                    break
+            webdriver.ActionChains(driver).move_to_element(sell_type).click(). \
+                send_keys(Keys.ESCAPE). \
+                perform()
+            for property in properties:
+                webdriver.ActionChains(driver).move_to_element(properties_pointer).click.perform()
+                for y in properties_pointer.find_elements(By.TAG_NAME, "li"):
+                    if y.text == property:
+                        property_type = y
+                        break
+                webdriver.ActionChains(driver).move_to_element(property_type).click(). \
+                    send_keys(Keys.ESCAPE). \
+                    move_to_element(search_button). \
+                    click(). \
+                    perform()
+                wait.until(EC.url_changes(origin_url))
+                #nxt_url = f"{curr_url}{sell}/{property}/"
+                max_iter = 100
+                types_list = [sell, property]
+                for j in range(max_iter):
+                    # This is the scrapping of just one page of the higher variables's configuration
+                    content = driver.page_source
+                    soup = BeautifulSoup(content, "html.parser")
+                    for house in soup.findAll("li", class_="ui-search-layout__item"):
+                        price_elem = house.find("span")
+                        price = [price_elem.text]
+                        features_elems = house.findAll("li")
+                        features = [elem.text for elem in features_elems]
+                        location_elem = house.findAll("p")
+                        location = [elem.text for elem in location_elem]
+                        all = [item.strip().lower()
+                               for sublist in [price, features, location, types_list]
+                               for item in sublist]
+                        rows.append(all)
+                    next_page_locator = By.XPATH, "//a[@title='Siguiente' and @role='button']"
+                    try:
+                        next_page = driver.find_element(
+                            *next_page_locator)  # Two elements with the same class, Previous and Next
+                        # location = {'x': 684, 'y': 10815}
+                        next_url = next_page.get_attribute("href")
+                    except NoSuchElementException:
+                        print(f"All pages scrapped from configuration {types_list[0]}-{types_list[1]}. \n"
+                              f"{j+1} pages have been screpped")
+                        break
+                    wait.until(EC.element_to_be_clickable(next_page_locator))
+                    webdriver.ActionChains(driver) \
+                        .move_to_element(next_page) \
+                        .click(next_page).perform()
+                    wait.until(
+                        EC.url_to_be(next_url))  # Waiting until it's at the url clicked
+                    wait.until(
+                        EC.presence_of_all_elements_located((By.TAG_NAME, "li")))  # until all list elements can be seen
+                    break
+                driver.get(origin_url)
+
+    df = pd.DataFrame(rows)
+    return df
+dataframe = scrapping_all()
+dataframe.to_csv("Data/all_house_data_raw.csv", index=False)
