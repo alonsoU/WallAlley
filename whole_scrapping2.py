c8f7bb3b2ac3bd418e6b1052798ddcd38cb0ed29
Alonso Uribe <a.uribe_@hotmail.com>
2021-03-06 13:25:21 -0300

    Ready.
    Some html's attribute pointing at the selling type's scrollbar that changes on clicking, to then presumebly restoring its original value on clicking again, presents some modifications from the original page. So is used another element as guide.
    
    Reseting all driver's webelement is done at the bottom of the second for loop (the one that is iterating through properties types).

--- whole_scrapping2.py
+++ whole_scrapping2.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import numpy as np
+import time
 
 def webelement_to_xpath(webelement, by):
     name = webelement.tag_name
@@ -18,7 +19,7 @@ def scrapping_all():
     with webdriver.Chrome("C:/Users/Alonso Uribe/AppData/Local/Chromium/User Data/chromedriver.exe") as driver:
         driver.get("https://www.portalinmobiliario.com/")
         origin_url = driver.current_url
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 30)
         cookies = driver.find_element(By.XPATH, "//button[@id='cookieDisclaimerButton']")
         try:
             webdriver.ActionChains(driver).click(cookies).perform()
@@ -33,14 +34,14 @@ def scrapping_all():
         except NoSuchElementException:
             pass
 
-        menu_class = "search-bar "
-        menu_xpath = f"//div[@class='{menu_class}']"
+        menu_class = "searchbox"
+        menu_xpath = f"//searchbox-realestate[@class='{menu_class}']"
         menu_pointer = driver.find_element(By.XPATH, menu_xpath)
-        pointers_class = "andes-dropdown andes-dropdown--standalone  searchbox-dropdown "
-        pointers_xpath = f"//div[@class='{pointers_class}']"
-        sells_pointer, properties_pointer = menu_pointer.find_elements(
-            By.XPATH,
-            pointers_xpath)
+        pointers_xpath = f"//searchbox-dropdown"
+        sells_pointer, properties_pointer = menu_pointer.find_elements(By.XPATH, pointers_xpath)
+        search_button = menu_pointer.find_element(
+            By.XPATH, "//searchbox-button[@id='search-submit']"
+        )
         # Collecting all the posible variables configuration,
         # then it's just needed to change the respective url
         sells = []
@@ -56,37 +57,32 @@ def scrapping_all():
             perform()
         driver.implicitly_wait(1)
         for property_type in properties_pointer.find_elements(By.TAG_NAME, 'li'):
-            properties.append(property_type.text.lower())
-        webdriver.ActionChains(driver).move_to_element(properties_pointer).send_keys(Keys.ESCAPE).perform()
+            properties.append(property_type.text)
+        webdriver.ActionChains(driver)\
+            .move_to_element(properties_pointer).send_keys(Keys.ESCAPE).perform()
 
         for sell in sells:
-            menu_pointer = driver.find_element(By.XPATH, menu_xpath)
-            search_button = menu_pointer.find_element(
-                By.XPATH, "//searchbox-button[@id='search-submit']"
-            )
-            sells_pointer, properties_pointer = menu_pointer.find_elements(By.XPATH, pointers_xpath)
-            print(sells_pointer)
             webdriver.ActionChains(driver).move_to_element(sells_pointer).click().perform()
             for x in sells_pointer.find_elements(By.TAG_NAME, "li"):
                 if x.text == sell:
                     sell_type = x
                     break
-            webdriver.ActionChains(driver).move_to_element(sell_type).click(). \
-                send_keys(Keys.ESCAPE). \
-                perform()
+            webdriver.ActionChains(driver).move_to_element(sell_type).click() \
+                .send_keys(Keys.ESCAPE) \
+                .perform()
             for property in properties:
-                webdriver.ActionChains(driver).move_to_element(properties_pointer).click.perform()
+                webdriver.ActionChains(driver).move_to_element(properties_pointer).click() \
+                    .perform()
                 for y in properties_pointer.find_elements(By.TAG_NAME, "li"):
                     if y.text == property:
                         property_type = y
                         break
-                webdriver.ActionChains(driver).move_to_element(property_type).click(). \
-                    send_keys(Keys.ESCAPE). \
-                    move_to_element(search_button). \
-                    click(). \
-                    perform()
+                webdriver.ActionChains(driver).move_to_element(property_type).click() \
+                    .send_keys(Keys.ESCAPE) \
+                    .move_to_element(search_button) \
+                    .click() \
+                    .perform()
                 wait.until(EC.url_changes(origin_url))
-                #nxt_url = f"{curr_url}{sell}/{property}/"
                 max_iter = 100
                 types_list = [sell, property]
                 for j in range(max_iter):
@@ -108,12 +104,11 @@ def scrapping_all():
                     try:
                         next_page = driver.find_element(
                             *next_page_locator)  # Two elements with the same class, Previous and Next
-                        # location = {'x': 684, 'y': 10815}
                         next_url = next_page.get_attribute("href")
                     except NoSuchElementException:
                         print(f"All pages scrapped from configuration {types_list[0]}-{types_list[1]}. \n"
                               f"{j+1} pages have been screpped")
-                        break
+                        break # Final page, there is no next button
                     wait.until(EC.element_to_be_clickable(next_page_locator))
                     webdriver.ActionChains(driver) \
                         .move_to_element(next_page) \
@@ -122,8 +117,17 @@ def scrapping_all():
                         EC.url_to_be(next_url))  # Waiting until it's at the url clicked
                     wait.until(
                         EC.presence_of_all_elements_located((By.TAG_NAME, "li")))  # until all list elements can be seen
-                    break
+                    #break # debugging
+                # Here, when some houses search of variable's configuration ends,
+                # the driver goes back to the main page(origin), where webelements are reset
+                # and the next variable's configuration's search start
                 driver.get(origin_url)
+                wait.until(EC.url_to_be(origin_url))
+                menu_pointer = driver.find_element(By.XPATH, menu_xpath)
+                search_button = menu_pointer.find_element(
+                    By.XPATH, "//searchbox-button[@id='search-submit']"
+                )
+                sells_pointer, properties_pointer = menu_pointer.find_elements(By.XPATH, pointers_xpath)
 
     df = pd.DataFrame(rows)
     return df
